980d15f7c590da55a7d6d0bb0b4b3bb2d963976c
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-06-21 17:10:42 +0100

    Initial Commit

---
+++ MyBooksApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBooksApp
+//
+//  Created by Umair Salam on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBooksApp/MyBooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksAppApp.swift
+//  MyBooksApp
+//
+//  Created by Umair Salam on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
